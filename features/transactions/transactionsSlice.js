8523aa5de072ebd0afeb04ad413ee1558b23b36c
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-06-15 17:33:55 +0000

    js | transactionsSlice file

---
+++ features/transactions/transactionsSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CATEGORIES = [
+  "housing",
+  "food",
+  "transportation",
+  "utilities",
+  "clothing",
+  "healthcare",
+  "personal",
+  "education",
+  "entertainment",
+];
+const initialState = Object.fromEntries(
+  CATEGORIES.map((category) => [category, []])
+);
+
+const transactionsSlice = createSlice({
+  name: "transactions",
+  initialState: initialState,
+  reducers: {
+    addTransaction: (state, action) => {
+      // add the new transaction object (action.payload) to the correct category’s transaction list in the transactions state object.
+      const category = action.payload.category;
+      state[category].push(action.payload);
+    },
+    // Add a deleteTransaction property
+    deleteTransaction: (state, action) => {
+      // In the deletedIndex in transactionsReducer, action.payload.category and action.payload.id are both used.
+      const id = action.payload.id;
+      const category = action.payload.category;
+
+      state[category] = state[category].filter(
+        (transaction) => transaction.id !== id
+      );
+    },
+  },
+});
+
+export const selectTransactions = (state) => state.transactions;
+export const selectFlattenedTransactions = (state) =>
+  Object.values(state.transactions).reduce((a, b) => [...a, ...b], []);
+
+export const { addTransaction, deleteTransaction } = transactionsSlice.actions;
+export default transactionsSlice.reducer;
